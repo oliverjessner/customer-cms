aadc86e44aa94a7a3fa31333219cc4237a172bd7
oliverjessner <oliver.jessner@gmail.com>
2023-11-23 12:28:54 +0100

    changing something

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hallo Welt');
+    res.send('<h1>Hallo Welt</h1>');
 });
 
 app.listen(3000);
